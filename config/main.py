7b5f8b46d7ba42a2d7452cfb2107eb7a07aa573e
reza drakhshan <github2reza@gmail.com>
2024-11-21 18:59:26 +0330

    perf
    now all domain can request to api

--- config/main.py
+++ config/main.py
@@ -5,6 +5,15 @@ from db.database import engine
 
 app = FastAPI(title="WorkOut")
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(send_code.router)
 app.include_router(auth.router)
